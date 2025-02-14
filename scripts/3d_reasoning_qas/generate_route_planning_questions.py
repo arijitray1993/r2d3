883d3cc1f6948ae7bf0d798bc8c6dea6bd725b51
Arijit Ray <array@bu.edu>
2025-02-14 18:50:44 -0500

    updated code

--- scripts/3d_reasoning_qas/generate_route_planning_questions.py
+++ scripts/3d_reasoning_qas/generate_route_planning_questions.py
@@ -2,6 +2,11 @@ from collections import defaultdict
 import os
 import h5py
 import cv2
+import numpy as np
+import random
+import json
+import ast
+
 
 def convert_byte_to_string(bytes_to_decode, max_len=None):
     if max_len is None:
@@ -37,6 +42,9 @@ def make_desc_for_task(task_name, task_templ):
 
 
 def get_current_state(controller):
+    objid2assetid = {}
+    for obj in controller.last_event.metadata['objects']:
+        objid2assetid[obj['objectId']] = obj['assetId']
     nav_visible_objects = controller.step("GetVisibleObjects", maxDistance=5).metadata["actionReturn"]
     nav_visible_objects = [obj for obj in nav_visible_objects if objid2assetid[obj]!=""] # these are the visible object asset ids in the scene
     
@@ -93,26 +101,30 @@ def get_current_state(controller):
 if __name__=="__main__":
 
     data_path = "/projectnb/ivc-ml/array/research/robotics/dreamworlds/custom_datasets/procThor/SPOC_data"
+    split = "train"
 
     tasks = os.listdir(data_path)
     action_map = {
         'm': "go straight",
         'l': "left",
         'r': "right",
-        'ls': "left",
-        'rs': "right",
+        'ls': "slight left",
+        'rs': "slight right",
         'b': "move backward",
         'end': "no action, goal achieved",
     }
     action_map = {
-        'move_ahead': "straight",
+        'move_ahead': "go straight",
         'rotate_left': "left",
         'rotate_right': "right",
-        'rotate_left_small': "left",
-        'rotate_right_small': "right",
+        'rotate_left_small': "slight left",
+        'rotate_right_small': "slight right",
     }
     selected_tasks = ["EasyObjectNavType"]#, "PickupType"] # "ObjectNavDescription",]
     action_choices = list(action_map.values())
+
+
+    dataset = prior.load_dataset("procthor-10k")
     
     for task in tasks:
         if task not in selected_tasks:
@@ -134,6 +146,7 @@ if __name__=="__main__":
                 # 'last_agent_location', 'minimum_l2_target_distance', 'minimum_visible_target_alignment', 'room_current_seen', 
                 # 'rooms_seen', 'task_relevant_object_bbox', 'templated_task_spec', 'visible_target_4m_count'
 
+                # convert the actions into strings -  they are stored in bytes. 
                 actions_taken = []
                 for action in episode['last_action_str']:
                     actions_taken.append(convert_byte_to_string(action))
@@ -146,37 +159,90 @@ if __name__=="__main__":
                 for action in episode['last_action_success']:
                     last_action_success.append(action[0])
 
-                #pdb.set_trace()
+                agent_locations = episode['last_agent_location']
+
+                # Whether target object is visible or not for each frame
                 object_bbox_cols = np.array(episode['nav_task_relevant_object_bbox']['max_cols'])
-                
                 obj_visible = [col[0]!=-1 for col in object_bbox_cols]
 
+                # make a description for the task
                 task_templ = ast.literal_eval(convert_byte_to_string(episode['templated_task_spec'][0]))
                 task_desc = make_desc_for_task(task, task_templ)
-                # pdb.set_trace()
+
+
+                # instantiate a controller in the house
+                house_ind = episode['house_index'][0]
+                house_json = dataset[split][house_ind]
                 
-                for index, (action, rand_action, last_action_succ, obj_vis) in enumerate(zip(actions_taken, random_action, last_action_success, obj_visible)):
-                    if obj_vis:
-                        if (not rand_action) and last_action_succ and index > 0:
-                            if action in action_map:
-                                action_word = action_map[action]
-                            else:
-                                action_word = action.replace("_", " ")
-                            # pdb.set_trace()
-                            if action_word == "look straight":
-                                if random.random() > 0.5:
-                                    continue
-                            if self.args['split'] == "val": # just for zero-shot eval.
-                                if action_word == "done":
-                                    continue
+                try:
+                    controller = Controller(scene=house_json, width=512, height=512, quality="Ultra", platform=CloudRendering,  renderInstanceSegmentation=True)
+                except:
+                    print("Cannot render environment, continuing")
+                    continue
+
+
+                for index, (action, rand_action, last_action_succ, obj_vis, agent_loc) in enumerate(zip(actions_taken, random_action, last_action_success, obj_visible, agent_locations)):
+                    
+                    if (not rand_action) and last_action_succ and index > 0:
+                        if action in action_map:
+                            action_word = action_map[action]
+                        else:
+                            action_word = action.replace("_", " ")
+
+                        # teleport the agent to the last location
+                        controller.step("Teleport", x=agent_loc[0], y=agent_loc[1], z=agent_loc[2], rotation=dict(x=0, y=agent_loc[3], z=0))
+
+                        if "straight" in action_word:
+                            continue
+                        else:
+                            # Get the closest object to controller
+                            nav_visible_objects, objid2info, objdesc2cnt, moveable_visible_objs = get_current_state(controller)
                             
-                                if not obj_visible:
-                                    continue
+                            closest_obj = None
+                            closest_dist = 1000
+                            for obj in objid2info:
+                                if objid2info[obj][2] < closest_dist:
+                                    closest_dist = objid2info[obj][2]
+                                    closest_obj = obj
                             
-                            self.all_data.append({
-                                'frame_index': index-1,
-                                'task': task_desc,
-                                'episode_id': episode_id,
-                                'video_file': video_file,
-                                'action_taken': action_word,
-                            })
+                            if closest_obj is None:
+                                continue
+
+                            action_so_far = f"go straight near {closest_obj}"
+
+                            
+                        
+
+
+    split = "train"
+    asset_id_desc = json.load(open("/projectnb/ivc-ml/array/research/robotics/dreamworlds/scripts/mturk_clean_assrt_desc/assetid_to_info.json", "r"))
+    qa_json_path = f'/projectnb/ivc-ml/array/research/robotics/dreamworlds/custom_datasets/procThor/route_planning_qas.json'
+    vis = True
+    stats = False
+    generate = False
+    load_progress = False
+
+    if generate:
+        if not os.path.exists(qa_im_path):
+            os.makedirs(qa_im_path)
+
+        assetid2desc = {}
+        for asset in asset_id_desc:
+            entries = asset_id_desc[asset]
+            captions = []
+            for im, obj, desc in entries:
+                desc = desc.strip().lower().replace(".", "")
+                captions.append(desc)
+            assetid2desc[asset] = random.choice(captions)
+
+
+        dataset = prior.load_dataset("procthor-10k")
+
+        all_im_qas = []
+        
+        if load_progress:
+            all_im_qas = json.load(open(qa_json_path, "r"))
+
+        for house_ind, house in enumerate(tqdm.tqdm(dataset[split])):
+            
+            house_json = house
